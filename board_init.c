e36ab83c7a85904807bf53770981e3665594a929
Airzone <airzone@ireland-soft.com>
2020-08-13 13:10:44 +1000

    Update board_init.c
    Updated for Narfduino

--- board_init.c
+++ board_init.c
@@ -1,6 +1,6 @@
 #include "py/mphal.h"
 
-void WeAct_F411CE_board_early_init(void) {
+void Narfduino_Pro_board_early_init(void) {
     // set SPI flash CS pin high
     mp_hal_pin_output(pin_A4);
     mp_hal_pin_write(pin_A4, 1);
